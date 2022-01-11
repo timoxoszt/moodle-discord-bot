c89b57ba4794ad0d6c7026ef9468431120b208e9
finxol <personnal@bottleneck.anonaddy.com>
2022-01-11 19:22:39 +0100

    add jsdoc + correct day index offset

--- getEvents.js
+++ getEvents.js
@@ -11,10 +11,18 @@ class Calendar {
     constructor() {
     }
 
+    /**
+     * Get a list of all saved events, of length 1 at minimum
+     * @returns {{inline: boolean, name: string, value: string}|*} list containing the events, formatted for use in a discord embed
+     */
     get events() {
-        return this.empty ? {name: "Rendu", value: "Pas de rendus pour cette semaine", inline: true} : this.#events;
+        return this.empty ? [{name: "Rendu", value: "Pas de rendus pour cette semaine", inline: true}] : this.#events;
     }
 
+    /**
+     * Check if there are upcoming events saved
+     * @returns {boolean} true if there are no events, false otherwise
+     */
     get empty() {
         return this.#events.length === 0;
     }
@@ -30,16 +38,29 @@ class Calendar {
         return  Math.ceil(( today.getDay() + 1 + numberOfDays) / 7);
     }
 
+    /**
+     * Fetch all the events for the next week
+     * @returns {Promise<void>} none
+     */
     async fetch_week() {
         let url = this.#url + "&preset_time=weeknow";
         await this.fetch_events(url);
     }
 
+    /**
+     * Fetch all the events for the next month
+     * @returns {Promise<void>}
+     */
     async fetch_month() {
         let url = this.#url + "&preset_time=monthnow";
         await this.fetch_events(url);
     }
 
+    /**
+     * Fetch all the events from a given url and parse them into a discord embed usable format
+     * @param url url from which to get the events
+     * @returns {Promise<void>} none
+     */
     async fetch_events(url) {
         let response = await fetch(url);
         let data = await response.text();
@@ -53,17 +74,13 @@ class Calendar {
             let days = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
             let months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
             let d = new Date(event.endDate);
-            let date = `${days[d.getDay()]} ${d.getDate()} ${months[d.getMonth()]} ${d.getHours()}:${d.getMinutes()}`;
+            let date = `${days[d.getDay() -1]} ${d.getDate()} ${months[d.getMonth()]} ${d.getHours()}:${d.getMinutes()}`;
             this.#events.push({
                 name: eventData.jCal["1"]["8"]["3"] === undefined ? "R1.0x - xx" : eventData.jCal["1"]["8"]["3"],
                 value: `*${event.summary}*\n**${date}**`,
                 inline: true
             });
         });
-    }
-
-    week_events() {
-        return this.calendar;
     }
 }
 
